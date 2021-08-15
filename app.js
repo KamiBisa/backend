3bb1da2a9b6429692a89d967001b98ce67be9813
Stanley Claudius <stanleyclaudiuss@gmail.com>
2021-08-15 20:16:06 +0700

    Re-push

--- app.js
+++ app.js
@@ -1,25 +1,4 @@
-const app = require('./server')
-const {Pool} = require('pg');
-
-const pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
-    ssl: {
-        rejectUnauthorized: false
-    }
-});
-
-app.get('/db', async (req, res) => {
-    try {
-        const client = await pool.connect();
-        const result = await client.query('SELECT * FROM test_table');
-        const results = { 'results': (result) ? result.rows : null};
-        res.render('pages/db', results );
-        client.release();
-    } catch (err) {
-        console.error(err);
-        res.send("Error " + err);
-    }
-})
+const app = require('./server');
 
 app.listen(process.env.PORT, () => {
     console.log(`Server is running on port ${process.env.PORT}`)
