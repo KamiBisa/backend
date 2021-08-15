236d86e4eeb41d8225cc0b7a88eb02cf6ea9f8b4
Stanley Claudius <stanleyclaudiuss@gmail.com>
2021-08-15 14:54:09 +0700

    Adding pg

--- app.js
+++ app.js
@@ -1,4 +1,25 @@
 const app = require('./server')
+const {Pool} = require('pg');
+
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
+});
+
+app.get('/db', async (req, res) => {
+    try {
+        const client = await pool.connect();
+        const result = await client.query('SELECT * FROM test_table');
+        const results = { 'results': (result) ? result.rows : null};
+        res.render('pages/db', results );
+        client.release();
+    } catch (err) {
+        console.error(err);
+        res.send("Error " + err);
+    }
+})
 
 app.listen(process.env.PORT, () => {
     console.log(`Server is running on port ${process.env.PORT}`)
