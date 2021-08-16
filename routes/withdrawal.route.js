7249fa389198cdb575edad1ec23dc48eed956f06
Stanley Claudius <stanleyclaudiuss@gmail.com>
2021-08-16 09:46:01 +0700

    Change request method to get for verifyWithdrawal function

--- routes/withdrawal.route.js
+++ routes/withdrawal.route.js
@@ -3,6 +3,6 @@ const withdrawalControllers = require('./../controllers/withdrawalControllers');
 const {isAuthenticated, authorizeRoles, isVerifiedFundraiser} = require('./../middlewares/auth');
 
 router.route('/postWithdrawDonationProgram/:programId').post(isAuthenticated, authorizeRoles('fundraiser'), isVerifiedFundraiser, withdrawalControllers.withdrawDonationProgram);
-router.route('/postVerifyWithdraw/:withdrawalId/:status').post(isAuthenticated, authorizeRoles('admin'), withdrawalControllers.verifyWithdraw);
+router.route('/postVerifyWithdraw/:withdrawalId/:status').get(isAuthenticated, authorizeRoles('admin'), withdrawalControllers.verifyWithdraw);
 
 module.exports = router;
