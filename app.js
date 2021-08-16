f46abfd6f578f95f61499c0200362aab41140a36
Stanley Claudius <stanleyclaudiuss@gmail.com>
2021-08-16 15:50:02 +0700

    Test

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 const app = require('./server');
 
+app.get('/', () => {
+    res.json({
+        success: true
+    })
+})
+
 app.listen(process.env.PORT, () => {
     console.log(`Server is running on port ${process.env.PORT}`)
 })
