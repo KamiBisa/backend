0e8113230041c2d0aeee2221b2131d29d015a4be
Stanley Claudius <stanleyclaudiuss@gmail.com>
2021-08-16 20:30:07 +0700

    Push back

--- models/donation_programs.model.js
+++ models/donation_programs.model.js
@@ -41,9 +41,7 @@ DonationProgram.findById = (programId, result) => {
 }
 
 DonationProgram.selectByVerify = (status, result) => {
-  console.log('hit 1');
   sql.query(`SELECT * FROM donation_programs WHERE is_verified = ${status}`, (err, res) => {
-    console.log('hit2');
     if (err) {
       console.log(`Error: ${err.message}`);
       result(err, null);
