a65e55e70e9d42347f979c8379f5b5632f778904
Stanley Claudius <stanleyclaudiuss@gmail.com>
2021-08-16 21:25:47 +0700

    Ignore all deployment process

--- app.js
+++ app.js
@@ -1,21 +1,4 @@
 const app = require('./server');
-const db = require('./models/db/db');
-
-app.get('/', (req, res) => {
-    db.query(`SELECT * FROM users;`)
-        .then(data => {
-            res.json({
-                success: true,
-                user: data
-            })
-        })
-        .catch(err => {
-            res.json(err);
-        })
-    // res.json({
-    //     success: true
-    // })
-})
 
 app.listen(process.env.PORT, () => {
     console.log(`Server is running on port ${process.env.PORT}`)
--- models/db/db.js
+++ models/db/db.js
@@ -1,30 +1,17 @@
-// const mysql = require('mysql');
-// require('dotenv').config({path:__dirname+'/../../config/.env'});
-// // require('./setup').initDB();
+const mysql = require('mysql');
+require('dotenv').config({path:__dirname+'/../../config/.env'});
+// require('./setup').initDB();
 
-// const connection = mysql.createConnection({
-//   host: process.env.DB_HOST,
-//   user: process.env.DB_USER,
-//   password: process.env.DB_PASSWORD,
-//   database: process.env.DB_NAME
-// })
-
-// connection.connect(error => {
-//   if (error) throw error;
-//   console.log('Successfully connected to database.');
-// })
-
-// module.exports = connection;
-
-const Client = require('pg').Client;
-
-let db;
-
-db = new Client({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false
-  }
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
 })
 
-module.exports = db;
+connection.connect(error => {
+  if (error) throw error;
+  console.log('Successfully connected to database.');
+})
+
+module.exports = connection;
